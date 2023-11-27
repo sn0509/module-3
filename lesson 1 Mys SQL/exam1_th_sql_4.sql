5bed3d05ae9b4bccc255bb83969ea00d492aed06
sn0509 <sn050920@.com>
2023-11-27 15:49:55 +0700

    add new file

---
+++ lesson 1 Mys SQL/exam1_th_sql_4.sql
@@ -0,0 +1,23 @@
+create database demo;
+
+use demo;
+
+create table Student(
+	id int,
+	name varchar(200),
+    age int,
+    country varchar(50)
+    );
+    
+create table Class(
+	id int,
+    name varchar(50)
+	);
+    
+create table Teacher(
+	id int,
+    name varchar(50),
+    age int,
+    country varchar(50)
+    );
+	
